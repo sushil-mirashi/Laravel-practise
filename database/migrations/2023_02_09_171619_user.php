7daaaecb7bbdfcd3489d575b075789d40d670b73
unknown <sushilmirashi123@gmail.com>
2023-02-09 23:06:48 +0530

    user table migration

--- database/migrations/2023_02_09_171619_user.php
+++ database/migrations/2023_02_09_171619_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class User extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        if (!Schema::hasTable('users')) {
+          Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('emailid')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password');;
+            $table->enum('role',['admin', 'manager', 'normal-user', 'subsribed-user']);
             $table->timestamps();
         });
+        }
+
     }
 
     /**
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
